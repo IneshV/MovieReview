a142df76cb8d91a02fd3843acb945834dc86dda3
Inesh V <ivytheswaran@wesleyan.edu>
2025-05-11 22:57:50 -0400

    Initial Commit

---
+++ src/main/java/dev/inesh/movies/MovieRepository.java
@@ -0,0 +1,7 @@
+package dev.inesh.movies;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+public interface MovieRepository extends MongoRepository<Movie, ObjectId> {
+}
