fb1db03afc44264dbe274e7734993961715ee266
Inesh V <ivytheswaran@wesleyan.edu>
2025-05-11 23:26:08 -0400

    Successfully printed all DB data

---
+++ src/main/java/dev/inesh/movies/MovieService.java
@@ -0,0 +1,17 @@
+package dev.inesh.movies;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+
+public class MovieService {
+    @Autowired
+    public MovieRepository movieRepository;
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+
+    }
+}
